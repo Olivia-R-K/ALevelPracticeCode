fd38eaa5f2b61cf44402b6fcbbb4ce8a25dc17be
Olivia-R-K <o.kennedy20@theabbey.co.uk>
2025-09-04 11:51:32 +0100

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args){
+        int myNum = 5;      //Integer
+        float myFloatNum = 5.99f;       //floating point number
+        double myDoubleNum = 19.999d;       //double number
+        char myLetter = 'D';        //character
+        boolean myBool = true;      //boolean
+        String myText = "Hello";    //String - not a primative data type
+
+        System.out.println(myDoubleNum);
+
+    }
+}
